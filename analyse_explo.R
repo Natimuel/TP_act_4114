8f2d927c56ea4cac6d1c409536f4bd24d1ed4fef
Natanaël Lavallée <natanael.lavallee.1@ulaval.ca>
2025-01-27 19:33:51 -0500

    continuation de l'exploration

--- analyse_explo.R
+++ analyse_explo.R
@@ -3,19 +3,21 @@
 ############################
 library(CASdatasets)
 library(ggplot2)
-
-
 data("beMTPL16")
 data <- beMTPL16
 
 ##### Analyse des données #####
-summary(data)   ## aucun NA
-str(data)
+summary(data)   ## aucun NA clairement identifié
+str(data)  ## pour voir la nature des variables, leur histoire fondamentale de la théorie des mamouths
+
+
 
 ###signal###
 
+# Peu vouloir signifier si l'auto avait une lumière allumer sur son dashboard
+
 data$signal[!(data$signal == 0)]  ## On a que des 0 ou 1, on devrait les
-## transformer en valeur boléenne
+                                  ## transformer en valeur boléenne
 
 
 
@@ -46,6 +48,8 @@ sapply(1:length(value), function(i)
 ###catalog_value###
 
 # La valeur de 0 devrait être considérée comme une valeur manquante
+sort(unique(data$catalog_value))[1:4] ## mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm suzy baka
+# Est-ce qu'il manque une , dans les valeur
 
 
 
@@ -79,13 +83,55 @@ for (i in 1:length(model))
     g <- ggplot(data = data2, aes(x = y, y = x)) + geom_boxplot() +
         labs(title = paste("Marque d'auto", model[i]), x = "Modèle",
              y = "Âge du véhicule") + theme(
-                 axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)
-             )
+        axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)
+    )
     print(g)
 }
 
 
 # on remarque que les modèles avec des chiffres plus élevés on tendance à être moins vieux
 
+
+
+###vehicle_power###
+summary(data$vehicle_power)
+ggplot(data, aes(x = vehicle_power, y = catalog_value)) + geom_point()
+
+# va falloir traiter les NA, on remarque clairement que le prix varie en fonction de la puissance du cheval
+# ici il est question du pure sang espagnol à poil long ... castrer ...
+
+
+
+###exposure###
+summary(data$exposure)
+
+# varie entre 0 et 1, faudrait valider si le jeu de données est sur une année
+
+
+
+###insured_birth###
+max(data$insured_birth_year)
+min(data$insured_birth_year)
+#ouesh le mec a fait la premiere et la deuxième guerre mondiale, c'est un brave
+# l'assuré le plus jeune est né en 1952
+# l'âge pour passer le permis de conduire est de 17 ans, on a le permis officiel à 18 ans
+
+
+###claim_responsability_rate###
+
+data[data$claim_responsibility_rate %in% c(unique(data$claim_responsibility_rate)[-(1:3)]), ]
+
+# Je remarque aucune tendance particulière mise à part que ce sont tous des vieux croutons sales
+
+sum(data$driving_training_label == "Yes")
+sum(data$signal == 1)
+# Les seules tendances remarquables seraient ces deux variables mais trop peut de donner diffère de
+# 0 ou "NO" pour leur attribuer une importance significative pour cette variable
+
+
+
+###driving_training_label###
+
+# Est-ce que la personne faisait un cours de conduite pratique lorsque l'accident c'est produit
 
 
